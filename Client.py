b300d235cdcd117ffeec5c75a77a54371906a756
Nedwardio <55397842+Nedwardio@users.noreply.github.com>
2021-10-25 14:53:52 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,15 +1,15 @@
 import socket
 import threading
 import os
-
+#initialise server details
 HOST = "::1"
 PORT = 6667
 
 nickname = input("Enter a nickname: ")
-
+#initialise socket
 user = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 user.connect((HOST,PORT))
-
+#Receive info and messages from the server
 def receiveFromServer():
     while True:
         try:
@@ -28,17 +28,18 @@ def receiveFromServer():
             user.close()
             break
 
-
+#initialise thread
 r_thread = threading.Thread(target=receiveFromServer)
 r_thread.start()
 
+#Send messages to server
 def sendToServer():
     while True:
         msg = input("")
         user.send(msg.encode('utf-8'))
 
 
-
+#initialise thread
 s_thread = threading.Thread(target=sendToServer)
 s_thread.start()
 
